248d8e8f0ace2f72756b3c8c141b55f32e1262f0
Betsy Altenburger <hjc6uh@virginia.edu>
2022-09-13 15:59:20 +0000

    Daily commit

--- src/mywork/hw2.py
+++ src/mywork/hw2.py
@@ -1,4 +1,5 @@
 # Be sure you've done pip install z3-solver
+from telnetlib import X3PAD
 from z3 import *
 
 
@@ -6,42 +7,25 @@ from z3 import *
 # point on a working program to solve some problem
 # of interest. Here the problem is to compute and
 # return a non-negative square root of argument, n 
-def homework_two():
+def hw2():
     
     
     # Create z3 variable(s) representing the unknown
     # Here, the unknown, x, is the square root of n.
-    X,Y,Z = Bools('X Y Z')
-    
-    
-    # Important: This is where you express what
-    # values count as solutions using propositional
-    # logic, but in the slightly different syntax
-    # of Z3 expressions.
+    X, Y, Z = Bools('X Y Z')
     s = Solver()
-    
-    # 1. X ∨ Y, X ⊢ ¬Y             -- affirming the disjunct
-
-    C1 = Implies(And(Or(X,Y),X), Not(Y))
+    # 1. X ∨ Y, X ⊢ ¬Y 
+    # As proposition in PL: ((X \/ Y) /\ X) -> ~Y
+    C1 = Implies(And(Or(X,Y),X),Not(Y))
     s.add(Not(C1))
+    # I believe it's not valid
+    print_result(s, 1)
     
-    
-    # Create a Z3 "SMT" solver object, and give it 
-    # the overall constraint to be solved, here C.
-    
-    
-    
-    # Run the Z3 model finder, capturing "sat"
-    # or "unsat" as the return value 
-    isSat = s.check()
-    
-    # If there's a model/solution return it 
-    if (isSat == unsat):
-        print("It is valid")
-    # otherwise return inconsistent value for error
+    # 2, 
+
+def print_result(s, question_number):
+    if (s.check() == unsat):
+        print("C" + str(question_number)+ " is valid.")
     else:
-            print("Here is a counter example: " + str(s.model()))
-
-# Set up and run the function and report its results
-s = homework_two()
-
+        print("C" + str(question_number) + " is not valid. Here is a counter-example: ", s.model())
+hw2()
